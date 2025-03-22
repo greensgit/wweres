e6716dd53900440661720cc86d044ff955071ca2
greensgit <rajgreensomr@gmail.com>
2025-03-22 11:59:10 +0530

    new

---
+++ src/org/dem/tes/Dummy.java
@@ -0,0 +1,32 @@
+package org.dem.tes;
+
+public class Dummy {
+
+public static void main(String[] args) {
+	
+	System.out.println("This is demo");
+	System.out.println("This is yuvaraj branch");
+}
+private void login() {
+	System.out.println("This is Azar branch");
+}
+
+private void addToKart() {
+	System.out.println("This is anbarasu working on add to kart");
+}
+private void payment() {
+	System.out.println("This is Anish - working on Payment module");
+}
+
+private void Confirmation() {
+	System.out.println("This is satham");
+}
+
+private void search() {
+System.out.println("This is Joe branch - working on search");
+}
+
+private void cancellation() {
+	System.out.println("This is deepti - working on cancellattion");
+}
+}
